239adcad1d0c546addf10ce29cf38fa589a7ceda
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-05 15:40:10 -0700

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const divisibleBy4 = year % 4 == 0;
+  const isCentury = year % 100 == 0;
+  const divisibleBy400 = year % 400 == 0;
 
+  if (divisibleBy4 && (!isCentury || divisibleBy400)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
