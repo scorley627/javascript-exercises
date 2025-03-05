726361e0f74292c2c6248a332f593f9cd7eb808b
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-05 15:51:09 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let tempInC = (tempInF - 32) * 5 / 9;
+  if (!Number.isInteger(tempInC)) tempInC = Number(tempInC.toFixed(1));
+  return tempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let tempInF = tempInC * 9 / 5 + 32;
+  if (!Number.isInteger(tempInF)) tempInF = Number(tempInF.toFixed(1));
+  return tempInF;
 };
 
 // Do not edit below this line
