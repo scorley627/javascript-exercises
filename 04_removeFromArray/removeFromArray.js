3066b1a8b375a9c19f50f9e2a4056e961dec39b9
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-05 15:23:50 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  let newArray = [];
+  for (const element of array)
+    if (!values.includes(element)) newArray.push(element);
+  return newArray;
 };
 
 // Do not edit below this line
