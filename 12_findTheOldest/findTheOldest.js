db50e5357cce2b4167fffcf4b9eef88273dedb59
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-10 12:49:18 -0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+  let oldest = people[0];
+  let oldestAge = getPersonAge(oldest);
+
+  for (let person of people.slice(1)) {
+    const age = getPersonAge(person);
+    if (age > oldestAge) { 
+      oldest = person;
+      oldestAge = age;
+    }
+  }
+  return oldest;
 };
+
+function getPersonAge(person) {
+  if ("yearOfDeath" in person) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } else {
+    return currentYear - person.yearOfBirth;
+  }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
