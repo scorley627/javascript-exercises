76b6add426b6543a7ebae345686d3807e25b18fb
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-10 11:49:27 -0600

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  text = text.replace(/[^\w]/g, "").toLowerCase();
 
+  let idx1 = 0;
+  let idx2 = text.length - 1;
+  while (idx1 < idx2) {
+    if (text.charAt(idx1) != text.charAt(idx2)) return false;
+    ++idx1;
+    --idx2;
+  }
+  return true;
 };
 
 // Do not edit below this line
