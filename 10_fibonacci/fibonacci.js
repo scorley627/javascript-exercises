1ddbbd50ec10ad58a2632250f11393ddfcfba836
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-10 12:03:40 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
 
+  let arr = [0, 1];
+  while (arr.length <= num)
+    arr.push(arr.slice(-2).reduce((total, num) => total + num));
+  return arr[num];
 };
 
 // Do not edit below this line
